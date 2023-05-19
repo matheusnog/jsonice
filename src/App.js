bf3404ef76f01856a4b84ae6e51909a82d9368fd
matheusnog <matheusnogueira450@gmail.com>
2023-05-18 22:33:49 -0300

    Teste usuário git

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
+      <Content />
       <Footer />
     </>
   );
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Content extends React.Component {
+    render() {
+        return (
+            <>
+                <span>Teste conteúdo</span>
+            </>
+        )
+    }
+}
+
+export default Content;
