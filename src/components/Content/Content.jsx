99a3ec45dc6f4b13c439743b4d905e18fe4e26c2
matheusnog <matheusnogueira450@gmail.com>
2023-05-18 22:48:10 -0300

    Importando MUI e react-bootstrap

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { TextareaAutosize } from '@mui/base';
+import { Container } from 'react-bootstrap';
 
 class Content extends React.Component {
     render() {
         return (
-            <>
-                <span>Teste conteúdo</span>
-            </>
+            <Container fluid="md">
+                <TextareaAutosize style={{width: '100%', height: 100}}/>
+            </Container>
         )
     }
 }
