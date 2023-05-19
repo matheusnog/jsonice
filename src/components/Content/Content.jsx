0390d709e0bc37b78f01b440cc224aafb56dc704
matheusnog <matheusnogueira450@gmail.com>
2023-05-18 23:08:24 -0300

    Transformando Content para functional component

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextareaAutosize } from '@mui/base';
-import { Container } from 'react-bootstrap';
+import { Container, FormLabel } from 'react-bootstrap';
 
-class Content extends React.Component {
-    render() {
-        return (
-            <Container fluid="md">
-                <TextareaAutosize style={{width: '100%', height: 100}}/>
-            </Container>
-        )
-    }
+const Content = () => {
+    const [json, setJson] = useState(null)
+    return (
+        <Container fluid="md" className='mt-3'>
+            <FormLabel>Insira o JSON aqui:</FormLabel>
+            <TextareaAutosize style={{ width: '100%', height: 100 }} value={json} onChange={(e) => setJson(e.target.value)} />
+            <FormLabel>JSON estilizado:</FormLabel>
+            <TextareaAutosize style={{ width: '100%', height: 100 }} />
+        </Container>
+    )
+
 }
 
 export default Content;
