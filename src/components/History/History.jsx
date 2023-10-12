40a594c8f9355bc3ea5861918026f81e61ea556c
matheusnog <matheusnogueira450@gmail.com>
2023-10-11 22:14:07 -0300

    Adicionando react-toastify

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Fab, List, ListItem, ListItemButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
+import { ToastContainer, toast } from 'react-toastify';
 
 const History = ({ setJson }) => {
 
@@ -13,6 +14,7 @@ const History = ({ setJson }) => {
 
     function loadJson(key) {
         setJson(localStorage.getItem(key))
+        toast("Carregado com sucesso!")
     }
 
     function removeJson(key) {
@@ -20,50 +22,75 @@ const History = ({ setJson }) => {
         setArrayHistory(Object.keys(localStorage))
     }
 
+    const notify = (message) => {
+        toast(message, {
+            position: 'bottom-right',
+            autoClose: 2000,
+            style: {
+                backgroundColor: '#fff',
+                color: '#000',
+            },
+        });
+    };
+
     return (
-        <List>
-            {arrayHistory.length == 0 ?
-                <div className='text-center'>
-                    <h5>Sem histórico</h5>
-                </div>
-                : arrayHistory.map((ak, index) => {
-                    return localStorage.getItem(ak) ?
-                        < ListItem key={"listitem" + ak} >
-                            <ListItemButton
-                                key={"listitembutton" + ak}
-                                color="neutral"
-                                disabled={false}
-                                selected={false}
-                                variant="soft"
-                            >
-                                <div style={{ width: '100%', overflow: 'hidden' }}>
-                                    <span>{localStorage.getItem(ak)}</span>
-                                </div>
-                                <div style={{ marginLeft: 6, marginRight: 4 }}>
-                                    <Fab
-                                        size="small"
-                                        color='success'
-                                        aria-label="add"
-                                        onClick={() => loadJson(ak)}
-                                    >
-                                        <CheckIcon />
-                                    </Fab>
-                                </div>
-                                <div>
-                                    <Fab
-                                        size="small"
-                                        color='error'
-                                        aria-label="add"
-                                        onClick={() => removeJson(ak)}
-                                    >
-                                        <DeleteIcon />
-                                    </Fab>
-                                </div>
-                            </ListItemButton>
-                        </ListItem>
-                        : <></>
-                })}
-        </List >
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <List>
+                {arrayHistory.length == 0 ?
+                    <div className='text-center'>
+                        <h5>Sem histórico</h5>
+                    </div>
+                    : arrayHistory.map((ak, index) => {
+                        return localStorage.getItem(ak) ?
+                            < ListItem key={"listitem" + ak} >
+                                <ListItemButton
+                                    key={"listitembutton" + ak}
+                                    color="neutral"
+                                    disabled={false}
+                                    selected={false}
+                                    variant="soft"
+                                >
+                                    <div style={{ width: '100%', overflow: 'hidden' }}>
+                                        <span>{localStorage.getItem(ak)}</span>
+                                    </div>
+                                    <div style={{ marginLeft: 6, marginRight: 4 }}>
+                                        <Fab
+                                            size="small"
+                                            color='success'
+                                            aria-label="add"
+                                            onClick={() => loadJson(ak)}
+                                        >
+                                            <CheckIcon />
+                                        </Fab>
+                                    </div>
+                                    <div>
+                                        <Fab
+                                            size="small"
+                                            color='error'
+                                            aria-label="add"
+                                            onClick={() => removeJson(ak)}
+                                        >
+                                            <DeleteIcon />
+                                        </Fab>
+                                    </div>
+                                </ListItemButton>
+                            </ListItem>
+                            : <></>
+                    })}
+            </List >
+        </>
     )
 
 }
